ce732cd2b099856ed5ed8d144249bac8c812d27c
Bartosz Mediger <cutme@users.noreply.github.com>
2020-09-27 12:04:47 +0200

    init

---
+++ src/js/helpers.js
@@ -0,0 +1,33 @@
+import { TweenLite, ScrollToPlugin } from "gsap/all";
+const scrollPlugin = ScrollToPlugin;
+
+(function(window, document, cutme, undefined) {
+
+    const Helpers = function() {
+        return {
+        	isInView: isInView,
+        	scrollTo: scrollTo
+        };
+    };   
+    
+    const isInView = function(el) {
+        let bottomOfWindow = (window.pageYOffset || window.scrollY) + window.innerHeight;
+        
+        if ( el.getBoundingClientRect().top + (window.pageYOffset || window.scrollY) < bottomOfWindow ) {
+            return true;
+        }
+    };
+    
+    const scrollTo = function (target, speed, offset) {    
+		TweenLite.to(window, speed, {
+			scrollTo: {
+				y: target - offset,
+				autoKill: false
+			},
+			ease: Power1.easeOut
+		});
+	};
+    
+    cutme.Helpers = new Helpers();
+
+}(window, document, window.cutme = window.cutme  || {}));
